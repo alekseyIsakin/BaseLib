48db1b74f3f3437e7465c206edeeb627a630fefd
alekseyIsakin <karahunforever@gmail.com>
2021-02-27 12:50:32 +0800

    Разделил репозтории проектов

---
+++ BaseElementsLib/AbstrPageEl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaseLib
+{
+    public abstract class AbstrPageEl
+    {
+        public int ID { get; protected set; }
+
+        public abstract void SetID(int id);
+
+        public abstract List<byte> GenSendData();
+        public abstract byte GetTypeEl();
+    }
+}
---
+++ BaseElementsLib/ExternalLib.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaseLib
+{
+    public class ExternalLib 
+    {
+        protected List<ExternalLibData> listELD = new List<ExternalLibData>();
+
+        public virtual List<ExternalLibData> getListElements()
+        { return listELD; }
+    }
+
+    public sealed class ExternalLibData
+    {
+        public readonly int id;
+        public readonly string name;
+        public readonly Func<AbstrPageEl> BaseConstruct;
+        public readonly Func<AbstrPageEl, AbstrUIBase> BaseUIConstruct;
+        public readonly Func<System.Xml.XmlNode, AbstrPageEl> XMLLoader;
+        public readonly Func<AbstrPageEl, System.Xml.XmlDocument, System.Xml.XmlElement> XMLWriter;
+        
+        public ExternalLibData(int id, string name,
+            Func<AbstrPageEl> BaseConstruct,
+            Func<AbstrPageEl, AbstrUIBase> BaseUIConstruct,
+            Func<System.Xml.XmlNode, AbstrPageEl> XMLLoader,
+            Func<AbstrPageEl, System.Xml.XmlDocument, System.Xml.XmlElement> XMLWriter)
+        {
+            this.id = id;
+            this.name = name;
+            this.BaseConstruct = BaseConstruct;
+            this.BaseUIConstruct = BaseUIConstruct;
+            this.XMLLoader = XMLLoader;
+            this.XMLWriter = XMLWriter;
+        }
+    }
+}
